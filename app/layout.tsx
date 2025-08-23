05703675306ec41d93e47fff5b49cd5582e39b32
Fred KISSIE <fredkiss3@gmail.com>
2025-08-23 04:53:22 +0200

    refactor

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
         <main className="text-white flex min-h-screen flex-col items-center justify-between p-4 pt-24 md:p-24 h-full bg-black">
           <div className="z-10 w-full h-full max-w-5xl items-center justify-between font-mono text-sm">
             <Link
-              href={`/`}
+              href="/"
               className="z-[10] fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit"
             >
               Go home
