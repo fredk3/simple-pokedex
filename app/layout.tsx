20f10ef20dd193ba54aa39cee240c90d5b50d4f2
Fred KISSIE <hello@fredkiss.dev>
2025-08-25 16:43:30 +0200

    ⚗️ test PR

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,23 +4,23 @@ import Link from "next/link";
 export const runtime = "edge";
 
 export default function RootLayout({ children }) {
-  return (
-    <html>
-      <head />
-      <body suppressHydrationWarning className="dark min-h-screen">
-        <main className="text-white flex min-h-screen flex-col items-center justify-between p-4 pt-24 md:p-24 h-full bg-black">
-          <div className="z-10 w-full h-full max-w-5xl items-center justify-between font-mono text-sm">
-            <Link
-              href={`/`}
-              className="z-[10] fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit"
-            >
-              Go home
-            </Link>
+	return (
+		<html>
+			<head />
+			<body suppressHydrationWarning className="dark min-h-screen">
+				<main className="text-white flex min-h-screen flex-col items-center justify-between p-4 pt-24 md:p-24 h-full bg-black">
+					<div className="z-10 w-full h-full max-w-5xl items-center justify-between font-mono text-sm">
+						<Link
+							href={`/`}
+							className="z-[10] fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit"
+						>
+							Hell from PR #3
+						</Link>
 
-            {children}
-          </div>
-        </main>
-      </body>
-    </html>
-  );
+						{children}
+					</div>
+				</main>
+			</body>
+		</html>
+	);
 }
